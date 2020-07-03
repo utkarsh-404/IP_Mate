0e5f33fb7e9dc22468d2183df18a7e0cec153ddb
mR. woRSt <45844052+UltraUtkarsh@users.noreply.github.com>
2020-07-03 15:22:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-apt install cowsay wget ruby nano toilet figlet curl -y
+apt install cowsay termux-api wget ruby nano toilet figlet curl -y
 gem install lolcat
 echo " "
 echo  -e "\e[32mNow you can use "
