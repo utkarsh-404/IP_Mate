9e74397f562ac3150c447c5ba91e8e8de65d0ec5
mR. woRSt <45844052+UltraUtkarsh@users.noreply.github.com>
2020-07-04 08:51:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ apt install cowsay termux-api wget ruby nano toilet figlet curl -y
 gem install lolcat
 echo " "
 echo  -e "\e[32mNow you can use "
-toilet -f mono12 -F border mR_iNFo
+toilet -f mono12 -F border IP_Mate
